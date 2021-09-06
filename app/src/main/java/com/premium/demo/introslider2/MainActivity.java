f777634a49e7a2b079f20ed288c38a2d1d3ae2ec
vishwesh <vishwesh.me17@gamil.com>
2021-09-06 12:42:49 +0530

    Initial commit

---
+++ app/src/main/java/com/premium/demo/introslider2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.premium.demo.introslider2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        PrefManager prefManager = new PrefManager(getApplicationContext());
+        if(prefManager.isFirstTimeLaunch()){
+            prefManager.setFirstTimeLaunch(false);
+            startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+            finish();
+        }
+    }
+    protected void btn_Click(View view){
+        Toast.makeText(MainActivity.this, "clicked on button", Toast.LENGTH_LONG).show();
+    }
+}
